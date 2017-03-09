32acebf1065a8a467057ba9a6952283fe7d87c5d
sarthakturkhia <sarthakturakhia@gmail.com>
2017-03-08 19:29:47 -0800

    Almost done

--- ui.R
+++ ui.R
@@ -61,8 +61,8 @@ ui <- fluidPage(
                                                    messageItem(
                                                      from = "Certified Nursing Assistant (CNA):",
                                                      message = HTML("Helps patients or clients with healthcare <br/>needs
-                        under the supervision of Registered <br/>Nurse (RN) or a 
-                        Practical Nurse <br/> (LPN)."),
+                        under the supervision of <br/> Registered Nurse (RN) or a <br/>
+                        Licensed Practical Nurse (LPN)."),
                                                      icon = icon("user-md")
                                                    ),
                                                    
@@ -101,36 +101,7 @@ ui <- fluidPage(
                       tabItems(
                         # First tab content
                         tabItem(tabName = "introduction",
-                                div (
-                                  HTML("Nursing Homes are one of the fastest growing practices in the country today. With 60,000 
-                                       nursing homes currently running in the country, it is difficult to find one that best suits everyone needs. The nursing home
-                                       finder is the end to all of your problems. This project is intended to ease the search process and help adults (25 and above) 
-                                       find a perfect nursing home for their parent(s). We have based this project on the official datasets available on the
-                                       <a href= 'https://www.medicare.gov/'> Medicare.gov </a> Nursing Home Compare Website provided by the Centers for Medicare & Medicaid Services. This dataset allows one to compare
-                                       the quality of care at every Medicare and Medicaid-certified Nursing Home currently running in the country.
-                                       <br/> <br/>
-                                       This tool will help you compare nursing homes in 50 states. Initially you would see the national map with a table 
-                                       showing the basic provider information(Name, Phone Number, Address, City, State, Fines and Overall Rating). 
-                                       You can choose a state from the drop down on the side, and you would see a close up map of your state, 
-                                       with location maps for each nursing home. You can hover over the location pins to see the names of the nursing homes. 
-                                       Below the map, the table is filtered to the specific state’s nursing homes. You can also filter the overall rating 
-                                       (from 1 to 5), and see the nursing homes with/without fines or all of them, according to your choices. 
-                                       This will further filter the location points and the table.
-                                       <br/> <br/>
-                                       You can select a specific nursing home at a time, and then toggle between the General Provider Information, 
-                                       Ratings and Penalties and Other Information for the specific nursing home on the side. The General Information tab 
-                                       will show the Name, Address and the Phone Number of the nursing homes. The Ratings and Penalties tab will show more 
-                                       specific ratings and penalties such as Overall Rating, Health Inspection Rating, Staffing Rating, 
-                                       Registered Nurse Staffing Rating and the Total Amount of fine due. The Other Information tab has 
-                                       information on the maximum capacity of residents(that is, the number of certified beds), the current residents, 
-                                       the reported hours out of expected hours for Certified Nursing Assistant(CNA), Licesened Practical Nurses (LPN),
-                                       Registered Nurses(RN) and for all nurses.
-                                       <br/> <br/>
-                                       You also have the ability to search the table and select the number of specific entires you would like to see in the table. 
-                                       There is a glossary available for you to help understand the difference between the different types of Nurses available and 
-                                       their respective information given in the table."
-                                  )
-                                )
+                                htmlOutput("introduction")
                         ),
                         tabItem(tabName = "datatable",
                                 tabBox(title = "Selection Summary", 
@@ -149,12 +120,12 @@ ui <- fluidPage(
                                 )
                         ),
                         tabItem(tabName = "graph",
-                                verticalLayout(
+                                # verticalLayout(
                                   box(title = textOutput("viz"),
                                       plotOutput("pie")),
                                   box(title = textOutput("viz2"),
                                       plotOutput("bar")))
-                        )
+                        # )
                       )
                   )
                 )
